90ee58508d6921d8dc302dc02220589562aff0b4
Wellerson de Castro <magewelle@live.com>
2022-02-25 01:57:35 -0300

    redefinição temática do tailwind e refinamento da navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '88pc': '88%',
+        '96pc': '96%',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '100%',
+          },
+          '@screen md': {
+            maxWidth: '100%',
+          },
+          '@screen lg': {
+            maxWidth: '100%',
+          },
+          '@screen xl': {
+            maxWidth: '100%',
+          },
+        }
+      })
+    }
+  ],
 };
