c1fe630371ae1e4e82d946cc06ed2eb413bb9987
Wellerson de Castro <magewelle@live.com>
2022-03-01 03:37:00 -0300

    mobile-first & imagens como background de divs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'hero': "url('gio.jpg')",
+        'sec2': "url('section2.jpg')",
+        'o1': "url('offer1.jpg')",
+        'o2': "url('offer2.jpg')",
+        'o3': "url('offer3.jpg')",
+        'o4': "url('offer4.jpg')",
+        'o5': "url('offer5.jpg')",
+        'o6': "url('offer6.jpg')",
+        'c1': "url('category1.jpg')",
+        'c2': "url('category2.jpg')",
+        'c3': "url('category3.jpg')",
+        'c4': "url('category4.jpg')",
+        'c5': "url('category5.jpg')",
+        'c6': "url('category6.jpg')",
+      },
       width: {
         '88pc': '88%',
         '96pc': '96%',
